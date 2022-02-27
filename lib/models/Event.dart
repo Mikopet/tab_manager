161b7923cbff4e40a9c222e658d403840ee3e6d9
Peter Mikola <mikopet@gmail.com>
2022-02-27 22:17:10 +0100

    configure amplify and add models

---
+++ lib/models/Event.dart
@@ -0,0 +1,217 @@
+/*
+* Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+// NOTE: This file is generated and may not follow lint rules defined in your app
+// Generated files can be excluded from analysis in analysis_options.yaml
+// For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
+
+// ignore_for_file: public_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_code
+
+import 'package:amplify_core/amplify_core.dart';
+import 'package:flutter/foundation.dart';
+
+
+/** This is an auto generated class representing the Event type in your schema. */
+@immutable
+class Event extends Model {
+  static const classType = const _EventModelType();
+  final String id;
+  final String? _name;
+  final TemporalDate? _start_date;
+  final TemporalDate? _end_date;
+  final TemporalDateTime? _createdAt;
+  final TemporalDateTime? _updatedAt;
+
+  @override
+  getInstanceType() => classType;
+  
+  @override
+  String getId() {
+    return id;
+  }
+  
+  String get name {
+    try {
+      return _name!;
+    } catch(e) {
+      throw new AmplifyCodeGenModelException(
+          AmplifyExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion:
+            AmplifyExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString()
+          );
+    }
+  }
+  
+  TemporalDate get start_date {
+    try {
+      return _start_date!;
+    } catch(e) {
+      throw new AmplifyCodeGenModelException(
+          AmplifyExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion:
+            AmplifyExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString()
+          );
+    }
+  }
+  
+  TemporalDate get end_date {
+    try {
+      return _end_date!;
+    } catch(e) {
+      throw new AmplifyCodeGenModelException(
+          AmplifyExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion:
+            AmplifyExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString()
+          );
+    }
+  }
+  
+  TemporalDateTime? get createdAt {
+    return _createdAt;
+  }
+  
+  TemporalDateTime? get updatedAt {
+    return _updatedAt;
+  }
+  
+  const Event._internal({required this.id, required name, required start_date, required end_date, createdAt, updatedAt}): _name = name, _start_date = start_date, _end_date = end_date, _createdAt = createdAt, _updatedAt = updatedAt;
+  
+  factory Event({String? id, required String name, required TemporalDate start_date, required TemporalDate end_date}) {
+    return Event._internal(
+      id: id == null ? UUID.getUUID() : id,
+      name: name,
+      start_date: start_date,
+      end_date: end_date);
+  }
+  
+  bool equals(Object other) {
+    return this == other;
+  }
+  
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is Event &&
+      id == other.id &&
+      _name == other._name &&
+      _start_date == other._start_date &&
+      _end_date == other._end_date;
+  }
+  
+  @override
+  int get hashCode => toString().hashCode;
+  
+  @override
+  String toString() {
+    var buffer = new StringBuffer();
+    
+    buffer.write("Event {");
+    buffer.write("id=" + "$id" + ", ");
+    buffer.write("name=" + "$_name" + ", ");
+    buffer.write("start_date=" + (_start_date != null ? _start_date!.format() : "null") + ", ");
+    buffer.write("end_date=" + (_end_date != null ? _end_date!.format() : "null") + ", ");
+    buffer.write("createdAt=" + (_createdAt != null ? _createdAt!.format() : "null") + ", ");
+    buffer.write("updatedAt=" + (_updatedAt != null ? _updatedAt!.format() : "null"));
+    buffer.write("}");
+    
+    return buffer.toString();
+  }
+  
+  Event copyWith({String? id, String? name, TemporalDate? start_date, TemporalDate? end_date}) {
+    return Event._internal(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      start_date: start_date ?? this.start_date,
+      end_date: end_date ?? this.end_date);
+  }
+  
+  Event.fromJson(Map<String, dynamic> json)  
+    : id = json['id'],
+      _name = json['name'],
+      _start_date = json['start_date'] != null ? TemporalDate.fromString(json['start_date']) : null,
+      _end_date = json['end_date'] != null ? TemporalDate.fromString(json['end_date']) : null,
+      _createdAt = json['createdAt'] != null ? TemporalDateTime.fromString(json['createdAt']) : null,
+      _updatedAt = json['updatedAt'] != null ? TemporalDateTime.fromString(json['updatedAt']) : null;
+  
+  Map<String, dynamic> toJson() => {
+    'id': id, 'name': _name, 'start_date': _start_date?.format(), 'end_date': _end_date?.format(), 'createdAt': _createdAt?.format(), 'updatedAt': _updatedAt?.format()
+  };
+
+  static final QueryField ID = QueryField(fieldName: "event.id");
+  static final QueryField NAME = QueryField(fieldName: "name");
+  static final QueryField START_DATE = QueryField(fieldName: "start_date");
+  static final QueryField END_DATE = QueryField(fieldName: "end_date");
+  static var schema = Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
+    modelSchemaDefinition.name = "Event";
+    modelSchemaDefinition.pluralName = "Events";
+    
+    modelSchemaDefinition.authRules = [
+      AuthRule(
+        authStrategy: AuthStrategy.PUBLIC,
+        operations: [
+          ModelOperation.CREATE,
+          ModelOperation.UPDATE,
+          ModelOperation.DELETE,
+          ModelOperation.READ
+        ])
+    ];
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.id());
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: Event.NAME,
+      isRequired: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: Event.START_DATE,
+      isRequired: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.date)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: Event.END_DATE,
+      isRequired: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.date)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
+      fieldName: 'createdAt',
+      isRequired: false,
+      isReadOnly: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
+      fieldName: 'updatedAt',
+      isRequired: false,
+      isReadOnly: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+  });
+}
+
+class _EventModelType extends ModelType<Event> {
+  const _EventModelType();
+  
+  @override
+  Event fromJson(Map<String, dynamic> jsonData) {
+    return Event.fromJson(jsonData);
+  }
+}
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,39 @@
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:tab_manager/src/pages/home_page.dart';
 import 'package:tab_manager/src/theme.dart';
 
-class TabManager extends StatelessWidget {
+import '../amplifyconfiguration.dart';
+import '../models/ModelProvider.dart';
+
+class TabManager extends StatefulWidget {
   const TabManager({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => AppState();
+}
+
+class AppState extends State<TabManager> {
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    Amplify.addPlugins([
+      AmplifyDataStore(modelProvider: ModelProvider.instance),
+      // AmplifyAPI(),
+    ]);
+    try {
+      await Amplify.configure(amplifyconfig);
+    } on AmplifyAlreadyConfiguredException {
+      print("Tried to reconfigure Amplify...");
+    }
+    // TODO: put this under a button inside a settings menu
+    Amplify.DataStore.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
