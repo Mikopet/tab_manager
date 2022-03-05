58788f31c7ffecb430b832926b292133a066b25c
Peter Mikola <mikopet@gmail.com>
2022-03-05 15:25:26 +0100

    upgrade drawer with some basic logic

--- lib/src/drawer.dart
+++ lib/src/drawer.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tab_manager/src/pages/events_page.dart';
 
 class NavDrawer extends StatelessWidget {
   const NavDrawer({Key? key}) : super(key: key);
+
+  void _navigateTo(c, page) {
+    Navigator.of(c).push(MaterialPageRoute(builder: (c) => page));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,27 +27,27 @@ class NavDrawer extends StatelessWidget {
                     image: AssetImage('assets/images/cover.webp'))),
           ),
           ListTile(
-            leading: const Icon(Icons.input),
-            title: const Text('Welcome'),
-            onTap: () => {},
+            leading: const Icon(Icons.event_rounded),
+            title: const Text('Events'),
+            onTap: () => _navigateTo(context, const EventsPage()),
           ),
           ListTile(
-            leading: const Icon(Icons.verified_user),
-            title: const Text('Profile'),
+            leading: const Icon(Icons.input_rounded),
+            title: const Text('Consumption'),
             onTap: () => {Navigator.of(context).pop()},
           ),
           ListTile(
-            leading: const Icon(Icons.settings),
+            leading: const Icon(Icons.book_rounded),
+            title: const Text('Manual'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings_rounded),
             title: const Text('Settings'),
             onTap: () => {Navigator.of(context).pop()},
           ),
           ListTile(
-            leading: const Icon(Icons.border_color),
-            title: const Text('Feedback'),
-            onTap: () => {Navigator.of(context).pop()},
-          ),
-          ListTile(
-            leading: const Icon(Icons.exit_to_app),
+            leading: const Icon(Icons.exit_to_app_rounded),
             title: const Text('Logout'),
             onTap: () => {Navigator.of(context).pop()},
           ),
