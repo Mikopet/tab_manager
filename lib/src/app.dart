df52ba38a2c4b5192acea2acf02652e8d28425b9
Peter Mikola <mikopet@gmail.com>
2022-03-14 18:22:33 +0100

    add cognito and a little code reorganization

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_datastore/amplify_datastore.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,9 @@ class TabManager extends StatefulWidget {
 }
 
 class AppState extends State<TabManager> {
+  static bool networkStatus = false;
+  static bool outboxStatus = false;
+
   @override
   void initState() {
     super.initState();
@@ -25,29 +29,27 @@ class AppState extends State<TabManager> {
     });
   }
 
-  static bool networkStatus = false;
-  static bool outboxStatus = false;
-
   void _configureAmplify(amplifyConfig) async {
-    Amplify.Hub.listen([HubChannel.DataStore], (hubEvent) {
-      if (hubEvent.eventName == 'networkStatus') {
-        setState(() {
-          networkStatus = hubEvent.payload as bool; // TODO: .active
-        });
-      }
-      if (hubEvent.eventName == 'outboxStatus') {
-        setState(() {
-          outboxStatus = hubEvent.payload as bool; // TODO: .isEmpty
-        });
-      }
-    });
-
     Amplify.addPlugins([
       AmplifyDataStore(modelProvider: ModelProvider.instance),
+      AmplifyAuthCognito(),
       // AmplifyAPI(),
     ]);
     try {
       await Amplify.configure(amplifyConfig);
+
+      Amplify.Hub.listen([HubChannel.DataStore], (hubEvent) {
+        if (hubEvent.eventName == 'networkStatus') {
+          setState(() {
+            // networkStatus = hubEvent.payload.active
+          });
+        }
+        if (hubEvent.eventName == 'outboxStatus') {
+          setState(() {
+            // outboxStatus = hubEvent.payload.isEmpty
+          });
+        }
+      });
     } on AmplifyAlreadyConfiguredException {
       print("Tried to reconfigure Amplify...");
     }
