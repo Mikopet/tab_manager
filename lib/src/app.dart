8fbb4eca742c391858525f76122bd9acecc15340
Peter Mikola <mikopet@gmail.com>
2022-03-23 10:12:13 +0100

    refactoring Amplify to backend provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,7 @@
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:amplify_datastore/amplify_datastore.dart';
-import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 
-import 'package:tab_manager/src/components/amplify_configuration_storage.dart';
 import 'package:tab_manager/src/pages/home_page.dart';
 import 'package:tab_manager/src/components/theme.dart';
-import 'package:tab_manager/models/ModelProvider.dart';
 
 class TabManager extends StatefulWidget {
   const TabManager({Key? key}) : super(key: key);
@@ -16,45 +11,6 @@ class TabManager extends StatefulWidget {
 }
 
 class AppState extends State<TabManager> {
-  static bool networkStatus = false;
-  static bool outboxStatus = false;
-
-  @override
-  void initState() {
-    super.initState();
-    AmplifyConfigurationStorage().readConfig().then((String value) {
-      if (value.isNotEmpty) {
-        _configureAmplify(value);
-      }
-    });
-  }
-
-  void _configureAmplify(amplifyConfig) async {
-    Amplify.addPlugins([
-      AmplifyDataStore(modelProvider: ModelProvider.instance),
-      AmplifyAuthCognito(),
-      // AmplifyAPI(),
-    ]);
-    try {
-      await Amplify.configure(amplifyConfig);
-
-      Amplify.Hub.listen([HubChannel.DataStore], (hubEvent) {
-        if (hubEvent.eventName == 'networkStatus') {
-          setState(() {
-            // networkStatus = hubEvent.payload.active
-          });
-        }
-        if (hubEvent.eventName == 'outboxStatus') {
-          setState(() {
-            // outboxStatus = hubEvent.payload.isEmpty
-          });
-        }
-      });
-    } on AmplifyAlreadyConfiguredException {
-      print("Tried to reconfigure Amplify...");
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/src/backend.dart
+++ lib/src/backend.dart
@@ -1,6 +1,10 @@
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 
 import 'package:tab_manager/src/pages/backend_page.dart';
+import 'package:tab_manager/src/components/amplify_configuration_storage.dart';
+import 'package:tab_manager/models/ModelProvider.dart';
 
 class BackendProvider extends StatefulWidget {
   const BackendProvider({Key? key, required this.child}) : super(key: key);
@@ -13,6 +17,53 @@ class BackendProvider extends StatefulWidget {
 
 class BackendState extends State<BackendProvider> {
   bool configStatus = false;
+
+  static bool networkStatus = false;
+  static bool outboxStatus = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    late final String amplifyConfig;
+
+    try {
+      AmplifyConfigurationStorage().readConfig().then((String value) {
+        amplifyConfig = value;
+
+        Amplify.addPlugin(
+            AmplifyDataStore(modelProvider: ModelProvider.instance));
+
+        if (amplifyConfig != '{}') {
+          Amplify.addPlugins([
+            // AmplifyAuthCognito(),
+            // AmplifyAPI(),
+          ]);
+          configStatus = true;
+        }
+
+        Amplify.configure(amplifyConfig);
+
+        Amplify.Hub.listen([HubChannel.DataStore], (hubEvent) {
+          if (hubEvent.eventName == 'networkStatus') {
+            setState(() {
+              // networkStatus = hubEvent.payload.active
+            });
+          }
+          if (hubEvent.eventName == 'outboxStatus') {
+            setState(() {
+              // outboxStatus = hubEvent.payload.isEmpty
+            });
+          }
+        });
+      });
+    } on AmplifyAlreadyConfiguredException {
+      print("Tried to reconfigure Amplify...");
+    }
+  }
 
   void stateRebuild(bool value) {
     configStatus = value;
